8c468af99c78b04a1575982673d7ee8dbbf45c0f
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-21 23:59:03 +0100

    refactor

--- Job.php
+++ Job.php
@@ -55,7 +55,7 @@ class Job{
     private function getTermometerValues($mac)
     {
         exec("timeout 10 gatttool -b $mac --char-write-req --handle='0x0038' --value=\"0100\" --listen | grep \"Notification handle\" -m 1", $response);
-        $values = explode(':', $response);
+        $values = explode(':', $response[0]);
         $result = explode(' ', $values[1]);
         return json_encode([
             'temperature' => hexdec($result[1].$result[0])/100,
