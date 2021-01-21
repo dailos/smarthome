bac5700e4173587031429f4d3a2a46142242e28e
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-22 00:21:58 +0100

    refactor

--- Job.php
+++ Job.php
@@ -55,8 +55,8 @@ class Job{
     private function getTermometerValues($mac)
     {
         exec("timeout 10 gatttool -b $mac --char-write-req --handle='0x0038' --value=\"0100\" --listen | grep \"Notification handle\" -m 1", $response);
-        if(!is_array($response) &&  strpos($response, '0x0036') !== false) {
-            $result = explode(' ', $response);
+        if(strpos($response[0], '0x0036') !== false) {
+            $result = explode(' ', $response[0]);
             return json_encode([
                 'temperature' => hexdec($result[7] . $result[6]) / 100,
                 'humidity' => hexdec($result[8]),
