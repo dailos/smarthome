7cf92d351d9a7b0db29b1c63c79593aa09d3e811
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-21 23:56:07 +0100

    refactor

--- Job.php
+++ Job.php
@@ -5,7 +5,6 @@ class Job{
     const TYPE_TERMOSTAT = 'termostat';
     const TYPE_TERMOMETER = 'termometer';
     const TERMOSTAT_SCRIPT='./Scripts/eq3.exp';
-    const TERMOMETER_SCRIPT='./Scripts/termometerWriter.sh';
     const FILE_PATH = './Data/';
     const REFRESH_HCI0_AT= ['10', '30', '50'];
     const DEVICES = [
@@ -38,19 +37,31 @@ class Job{
             sleep(2);
         }
         foreach (self::DEVICES as $device){
+            $status = null;
             switch ($device['type']) {
                 case self::TYPE_TERMOSTAT:
-
                     exec(self::TERMOSTAT_SCRIPT . " ". $device['mac'] . " devjson", $status);
-                    if($status){
-                        file_put_contents(self::FILE_PATH . $device['mac'] .'.json', $status);
-                    }
                     break;
                 case self::TYPE_TERMOMETER:
-                    exec(self::TERMOMETER_SCRIPT . " ". $device['mac']);
+                    $status = $this->getTermometerValues($device['mac']);
                     break;
             }
+            if($status){
+                file_put_contents(self::FILE_PATH . $device['mac'] .'.json', $status);
+            }
         }
+    }
+
+    private function getTermometerValues($mac)
+    {
+        exec("timeout 10 gatttool -b $mac --char-write-req --handle='0x0038' --value=\"0100\" --listen | grep \"Notification handle\" -m 1", $response);
+        $values = explode(':', $response);
+        $result = explode(' ', $values[1]);
+        return json_encode([
+            'temperature' => hexdec($result[1].$result[0])/100,
+            'humidity' => hexdec($result[2]),
+            'battery' => hexdec($result[4].$result[3])/1000
+        ]);
     }
 }
 
