cfe1194bfe38b7c34fe8412c89687aa2a60d8006
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-27 17:59:55 +0100

    fix host name

--- src/Config.php
+++ src/Config.php
@@ -6,7 +6,7 @@ class Config
 {
     const TYPE_TERMOSTAT = 'termostat';
     const TYPE_TERMOMETER = 'termometer';
-    const BROKER= 'volumio';
+    const BROKER= 'volumio.local';
     const TERMOSTAT_SCRIPT = __DIR__."/../bin/eq3.exp";
     const REFRESH_HCI0_AT= ['10', '30', '50'];
 
