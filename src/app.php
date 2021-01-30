663e433de14dfb529316491d857e1e48822a5c86
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-30 03:32:03 +0100

    fix publisher

--- src/app.php
+++ src/app.php
@@ -1,14 +1,14 @@
 <?php
-namespace Smarthome\EQ3;
+namespace Smarthome;
 
 use PhpMqtt\Client\MQTTClient;
 use PhpMqtt\Client\Exceptions\ConnectingToBrokerFailedException;
 
-class Device
+class App
 {
     const BROKER = "volumio.local";
-    const SCRIPT = __DIR__ . "/script.exp 00:1A:22:12:DF:0E ";    
-    const TERMOMETER_PUBLISHER = "sudo python ". __DIR__ . "/../Mijia/mijia.py";
+    const TERMOSTAT_SCRIPT = __DIR__ . "/EQ3/script.exp 00:1A:22:12:DF:0E ";    
+    const TERMOMETER_SCRIPT = "sudo python ". __DIR__ . "/Mijia/mijia.py";
 
     private $mqtt;    
 
@@ -26,17 +26,35 @@ class Device
     public function subscribe()
     {
         $this->mqtt->subscribe("erik/termostat/set",function ($topic, $command)  {           
-            shell_exec(self::SCRIPT . $command);
+            shell_exec(self::TERMOSTAT_SCRIPT . $command);
         }, 0);
         $this->mqtt->loop(true);     
     }
 
     public function publish()
     {              
-        exec(self::SCRIPT . "devjson", $status);
+        exec(self::TERMOSTAT_SCRIPT . "devjson", $status);
         $status = implode(' ', $status);       
         $this->mqtt->publish("erik/termostat/status", $status);
         $this->mqtt->close();
-        shell_exec(self::TERMOMETER_PUBLISHER);        
+        shell_exec(self::TERMOMETER_SCRIPT);        
     }
+}
+
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+define("PUBLISH", "publish");
+define("SUBSCRIBE","subscribe");
+
+$app = new App();
+
+switch ($argv[1]){
+    case PUBLISH:
+        $app->publish();
+        break;
+    case SUBSCRIBE:
+        $app->subscribe();
+        break;
 }
