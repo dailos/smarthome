b9714bb4bf5788b13245b58ccd478415c706490c
Dailos Marrero <dailos.marrero@westwing.de>
2021-01-31 16:58:25 +0100

    remove fork

--- src/app.php
+++ src/app.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $client = new Client();
 
 if(isset($argv[1]) && $argv == 'subscribe'){
-    $client->subcribe();
+    $client->subscribe();
 }else{
     $core = new Core($client);
     $core();
