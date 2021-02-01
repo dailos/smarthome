dbef794558aa72eb56d7669b146c0481345e210a
Dailos Marrero <dailos.marrero@westwing.de>
2021-02-01 01:03:48 +0100

    interruption base on file creation

--- src/app.php
+++ src/app.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $client = new Client();
 
-if(isset($argv[1]) && $argv == 'subscribe'){
+if(isset($argv[1]) && $argv[1] == 'subscribe'){
     $client->subscribe();
 }else{
     $core = new Core($client);
